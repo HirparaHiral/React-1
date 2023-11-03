e796f5e7e3f053412c0789cbe323c5edcab27887
Hirpara Hiral <hiralhirpara08@gmail.com>
2023-11-03 19:29:06 -0400

    soluation

---
+++ lab08/src/components/Cart.js
@@ -0,0 +1,20 @@
+import CartItem from "./CartItem";
+import styles from "./CartItem.module.css";
+
+function Cart(props) {
+  return (
+    <div className={styles.cart} data-testid="cart">
+      <h2>Cart</h2>
+      {props.cartItems.map((item) => (
+        <CartItem key={item.id} {...item} />
+      ))}
+      Total: $
+      {props.cartItems.reduce(
+        (prev, cartItem) => prev + parseFloat(cartItem.price),
+        0
+      )}
+    </div>
+  );
+}
+
+export default Cart;
